2b40eedbc3763cbd5f977548019f85d4210ac90c
matrayu <northridge614@gmail.com>
2019-03-23 06:35:59 -0700

    Update App.js
    Moved Summary and CustomizationForm to to be a dependency

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Summary from './Summary';
+import CustomizationForm from './CustomizationForm';
 import './App.css';
 
 class App extends Component {
@@ -46,87 +48,6 @@ class App extends Component {
       </div>
     );
   }
-}
-
-class CustomizationForm extends Component {
-  updateFeature(feature, newValue) {
-    const selected = Object.assign({}, this.props.selected);
-    selected[feature] = newValue;
-    this.props.handleUpdate(selected);
-  }
-
-  render() {
-    const features = Object.keys(this.props.features).map((itm, itmIndex) => {
-      const options = this.props.features[itm].map((opt, optIndex) => {
-        const selectedClass = opt.name === this.props.selected[itm].name ? 'feature__selected' : '';
-        const featureClass = 'feature__option ' + selectedClass;
-        return (
-          <li className='feature__item' key={optIndex}>
-            <div className={featureClass} onClick={e => this.updateFeature(itm, opt)}>
-              {opt.name}
-              ({ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-                .format(opt.cost) })
-            </div>
-          </li>
-        )
-      }) 
-      return (
-        <div className="feature" key={itmIndex}>
-          <div className="feature__name">
-            {itm}
-          </div>
-          <ul className="feature__list">
-            { options }
-          </ul>
-        </div>
-      )
-    })
-
-    return (
-      <section className="main__form">
-        <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
-        {features}
-      </section>
-    )
-  }
-}
-
-class Summary extends Component {
-  render() {
-    const summary = Object.keys(this.props.selected).map(key => (
-      <div className="summary__option" key={key}>
-        <div className="summary__option__label">
-          {key}
-        </div>
-        <div className="summary__option__value">
-          {this.props.selected[key].name}
-        </div>
-        <div className="summary__option__cost">
-          { new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-            .format(this.props.selected[key].cost) }
-        </div>
-      </div>
-    ))
-
-    const total = Object.keys(this.props.selected)
-      .reduce((acc, curr) => acc + this.props.selected[curr].cost, 0
-    )
-
-    return (
-      <section className="main__summary">
-        <h3>NEW GREENLEAF 2018</h3>
-          { summary }
-        <div className="summary__total">
-          <div className="summary__total__label">Your Price: </div>
-          <div className="summary__total__value">
-            { total }
-          </div>
-        </div>
-      </section>
-    );
-  }
-}
-
-
+};
 
 export default App;  
