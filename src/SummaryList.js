ff652ee37bd6c3544ef6468bb316f943c2ca80a0
matrayu <northridge614@gmail.com>
2019-03-23 06:37:33 -0700

    Create SummaryList.js

---
+++ src/SummaryList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './SummaryList.css';
+
+class SummaryList extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    };
+};
+
+export default SummaryList;
